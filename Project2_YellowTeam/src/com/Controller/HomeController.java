842529db461c90881d47517cf009552f93f803d9
bruke291 <caspervn29@gmail.com>
2017-11-16 10:08:49 +0700

    Tuân
    Thay đổi config file + thêm package + đổi tên package

---
+++ Project2_YellowTeam/src/com/Controller/HomeController.java
@@ -0,0 +1,65 @@
+package com.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "")
+	public String index1() {
+		return "user/index";
+	}
+	
+	@RequestMapping(value = "/")
+	public String index2() {
+		return "user/index";
+	}
+	
+	@RequestMapping(value = "index")
+	public String index3() {
+		return "user/index";
+	}
+	
+	@RequestMapping(value = "products")
+	public String products() {
+		return "user/products";
+	}
+	
+	@RequestMapping(value = "404")
+	public String eror404() {
+		return "user/404";
+	}
+	
+	
+	@RequestMapping(value = "checkout")
+	public String checkout() {
+		return "user/checkout";
+	}
+	
+	@RequestMapping(value = "contact")
+	public String contact() {
+		return "user/contact";
+	}
+	
+	@RequestMapping(value = "decor")
+	public String decor() {
+		return "user/decor";
+	}
+	
+	@RequestMapping(value = "health")
+	public String health() {
+		return "user/health";
+	}
+	
+	@RequestMapping(value = "mobile")
+	public String mobile() {
+		return "user/mobile";
+	}
+	
+	@RequestMapping(value = "single")
+	public String single() {
+		return "user/single";
+	}
+	
+}
